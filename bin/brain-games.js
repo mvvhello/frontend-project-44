fd0072803ca8c38bda41900592a54fc16fb3a9b6
merkulov <mvvhello@gmail.com>
2024-08-05 12:11:58 -0300

    delete unnecessary lines in brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
 import askNameAndGreet from "../src/cli.js";
-//import brainEvenGame from "./brain-even.js";
 
 askNameAndGreet();
-
-// const user = askNameAndGreet();
-// brainEvenGame(user) - is commented for ignoring it
