c4844b8d4250512bb2bb20c4ea24aaa056b7b3d0
merkulov <mvvhello@gmail.com>
2024-08-05 12:10:01 -0300

    refactor greetings

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,7 +3,6 @@
 import askNameAndGreet from "../src/cli.js";
 //import brainEvenGame from "./brain-even.js";
 
-console.log("Welcome to the Brain Games");
 askNameAndGreet();
 
 // const user = askNameAndGreet();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from "readline-sync";
 
 const askNameAndGreet = () => {
+  console.log(`Welcome to the Brain Games!`);
   const name = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${name}!`);
   return name; // return the name after printing for further usage
